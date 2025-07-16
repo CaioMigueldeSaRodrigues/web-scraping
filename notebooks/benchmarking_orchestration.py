8cfd993e9bea4e80280e6dcb23313470b2092ab5
Caio Rodrigues <19880@bemol.local>
2025-07-16 15:43:31 -0400

    Ajusta importação de sys e os em uma linha no notebook benchmarking_orchestration.py

--- notebooks/benchmarking_orchestration.py
+++ notebooks/benchmarking_orchestration.py
@@ -10,11 +10,7 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 # DBTITLE 2,Setup Environment and Parameters
-import sys
-import os
-from pyspark.sql import SparkSession
-import pandas as pd
-
+import sys, os
 project_root = os.path.abspath(os.path.join(os.getcwd(), '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
